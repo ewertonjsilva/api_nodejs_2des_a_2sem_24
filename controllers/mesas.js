a1ae22a9f4d149bfe61740a5001f2e393e41b349
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-09-12 11:38:06 -0300

    12-09 manha

--- controllers/mesas.js
+++ controllers/mesas.js
@@ -2,11 +2,15 @@ const db = require('../database/connection');
 
 module.exports = {
     async listarMesas(request, response) {
-        try {            
+        try {    
+            
+            const sql = `SELECT mes_id, mes_nome, mes_status, mes_lugares, ped_id FROM mesas ORDER BY mes_id ASC;`;
+            const mesas = await db.query(sql);    
+
             return response.status(200).json({
                 sucesso: true, 
                 mensagem: 'Lista de mesas.', 
-                dados: null
+                dados: mesas[0]
             });
         } catch (error) {
             return response.status(500).json({
